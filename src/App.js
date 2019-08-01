74a1e6bfb4bb4516d8068af4c53ebbf8353369ae
Sergio Cabral <sergioantonio60@gmail.com>
2019-08-01 14:01:07 -0400

    Adds BookItem, Initial state, and componentDidMount

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import {Route} from 'react-router-dom'
 import './App.css'
+import BookItem from './BookItem'
 
 class BooksApp extends React.Component {
   state = {
+    books:[],
+    shelves: [
+      {id:'currentlyReading', title: 'Currently Reading'},
+      {id:'wantToRead', title:'Want To Read'},
+      {id:'read', title:'Read'}
+    ]
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => {
+        this.setState(()=>({
+          books
+        }))
+      })
   }
 
   render() {
