8924031543c637c6e553c609279b110d9eb964e9
Sergio Cabral <sergioantonio60@gmail.com>
2019-08-01 14:27:31 -0400

    Adds 'li' to BookItem implementation

--- src/BookItem.js
+++ src/BookItem.js
@@ -4,24 +4,26 @@ import PropTypes from 'prop-types'
 const BookItem = props => {
   const {cover, shelf, title, author} = props
   return (
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" >
-        <img alt={''} src={cover} style={{display:'inherit'}} />
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" >
+          <img alt={''} src={cover} style={{display:'inherit'}} />
+          </div>
+          <div className="book-shelf-changer">
+            <select value={shelf} onChange={(event)=>{console.log('onChange: ',event.target.value)}}>
+              <option value="move" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
         </div>
-        <div className="book-shelf-changer">
-          <select value={shelf} onChange={(event)=>{console.log('onChange: ',event.target.value)}}>
-            <option value="move" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
-        </div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{author}</div>
       </div>
-      <div className="book-title">{title}</div>
-      <div className="book-authors">{author}</div>
-    </div>
+    </li>
   )
 }
 
