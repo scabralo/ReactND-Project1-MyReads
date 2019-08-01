d2eac8be7e0ff3dff27389808ae009512c21055b
Sergio Cabral <sergioantonio60@gmail.com>
2019-08-01 14:25:39 -0400

    Adds BooksList implementation and BookItem's

--- src/App.js
+++ src/App.js
@@ -37,10 +37,8 @@ class BooksApp extends React.Component {
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
+                    <BooksList books={this.state.books} />
                     <ol className="books-grid">
-                      <li>
-                        <BookItem title={'To Kill a Mockingbird'} author={'Harper Lee'} cover={'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'} shelf={'currentlyReading'} />
-                      </li>
                       <li>
                         <div className="book">
                           <div className="book-top">
